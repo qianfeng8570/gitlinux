b6e2eae409f62336bc704f5e3499ac0cce52bf7e
qianfeng <857091291@qq.com>
2016-11-01 21:10:14 +0800

    80

--- servers/app.js
+++ servers/app.js
@@ -6,5 +6,5 @@ var path = require("path");
 var saticpath = path.join(__dirname,"../static");
 app.use(express.static(saticpath));
 app.listen(80,function(){
-	console.log("open http://127.0.0.1:3000");
+	console.log("open http://127.0.0.1:80");
 })
