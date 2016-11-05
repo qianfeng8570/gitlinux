d40ceca5a9899bd936253cc41dbec0bde60d0860
qianfeng <857091291@qq.com>
2016-11-05 18:13:30 +0800

    fenxiangtest

--- servers/config.js
+++ servers/config.js
@@ -4,11 +4,21 @@
  * 注：测试时请替换您真实的数据
  */
 module.exports = function(){
-	return {
+	//个人订阅号
+	/*return {
 		//AppID(应用ID)
 		appid: 'wxd9eb57efcb88560b',
 		//AppSecret(应用密钥)
 		appsecret: '8e34f2661e0aee786dc4b17d0f781a42',
+		//Token(令牌)
+		token: 'gzh1062'
+	};*/
+	//微信测试号
+	return {
+		//AppID(应用ID)
+		appid: 'wx6349e7870f8e8c98',
+		//AppSecret(应用密钥)
+		appsecret: '15dd1065a4667d74540ac35bb8043c58',
 		//Token(令牌)
 		token: 'gzh1062'
 	};
