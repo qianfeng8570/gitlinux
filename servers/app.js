9644aadd70454a19d49fa211eae070b6aea9d701
qianfeng <857091291@qq.com>
2016-11-05 13:56:12 +0800

    fa

--- servers/app.js
+++ servers/app.js
@@ -1,9 +1,10 @@
 var express = require("express");
-
+var bodyparser = require("body-parser");
 var app = new express();
-
+app.use(bodyparser.json());
 var path = require("path");
 require("./token.js")(app);
+require("./jssdk.js")(app);
 var saticpath = path.join(__dirname,"../static");
 app.use(express.static(saticpath));
 app.listen(80,function(){
